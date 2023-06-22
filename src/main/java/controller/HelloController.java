1dce87ae7d30efa18c3dcbdf3bd1157a95ec7d43
Karen <mingki080808@gmail.com>
2023-06-23 01:21:20 +0900

    홈화면 뜨도록 변경

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
