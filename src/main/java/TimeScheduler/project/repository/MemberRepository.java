bdc7db818505c06da561791e949ec767a9d4a120
Karen <mingki080808@gmail.com>
2023-06-23 15:22:38 +0900

    구현체 완성

--- src/main/java/TimeScheduler/project/repository/MemberRepository.java
+++ src/main/java/TimeScheduler/project/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import TimeScheduler.project.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
+public interface MemberRepository {//db 아직 결정되지 않아서 interface 사용
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByEmail(String email);
