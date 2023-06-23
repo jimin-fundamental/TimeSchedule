99869bd56e4898532c6f89e9308c722c71d1e694
Karen <mingki080808@gmail.com>
2023-06-23 15:07:16 +0900

    Create MemberRepository.java

---
+++ src/main/java/TimeScheduler/project/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package TimeScheduler.project.repository;
+
+import TimeScheduler.project.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByEmail(String email);
+    List<Member> findAll();
+}
