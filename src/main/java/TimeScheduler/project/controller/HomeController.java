0520333e93f67064709bcf4cdbfacdf9a459d46b
Karen <mingki080808@gmail.com>
2023-06-25 22:28:32 +0900

    thymeleaf 형식으로 모두 바꿈

--- src/main/java/TimeScheduler/project/controller/HomeController.java
+++ src/main/java/TimeScheduler/project/controller/HomeController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "signup";
+        return "firstpage";
     }
+
 
 }
