fe86f41f92350c3295ca16d4d3d3094d4d4852e8
Karen <mingki080808@gmail.com>
2023-06-25 22:57:08 +0900

    홈화면 나오게 & 4초 후 자동으로 login 페이지로 넘어가게

--- src/main/java/TimeScheduler/project/controller/MemberController.java
+++ src/main/java/TimeScheduler/project/controller/MemberController.java
@@ -3,6 +3,7 @@ package TimeScheduler.project.controller;
 import TimeScheduler.project.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -12,4 +13,11 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/login")
+    public String createForm() {
+        return "login";
+    }
+
+
 }
