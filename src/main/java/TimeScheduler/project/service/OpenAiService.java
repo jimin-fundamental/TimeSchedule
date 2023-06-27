3cde768f42207f85e79a94b2b99a21efe61c0d58
Karen <mingki080808@gmail.com>
2023-06-27 19:14:24 +0900

    프롬포트 엔지니어링 중

--- src/main/java/TimeScheduler/project/service/OpenAiService.java
+++ src/main/java/TimeScheduler/project/service/OpenAiService.java
@@ -7,7 +7,10 @@ import org.springframework.transaction.annotation.Transactional;
 import TimeScheduler.project.controller.Task;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Transactional
 public class OpenAiService {
@@ -19,115 +22,112 @@ public class OpenAiService {
 
     public OpenAiService() {
         this.apiKey = API_KEY;
-        this.client = new OkHttpClient();
+        // Create OkHttpClient with a timeout of 300 seconds
+        this.client = new OkHttpClient.Builder()
+                .connectTimeout(Duration.ofSeconds(300))
+                .build();
     }
+
 
     public void fetchUpdatedSchedule(List<Task> fixedTasks, List<Task> flexibleTasks) throws IOException {
-        JSONArray messagesArray = new JSONArray();
+            JSONArray messagesArray = new JSONArray();
 
-        // Add system message for meal time constraint
-        JSONObject mealTimeSystemMessage = new JSONObject();
-        mealTimeSystemMessage.put("role", "system");
-        mealTimeSystemMessage.put("content", "You are a helpful assistant.");
-        messagesArray.put(mealTimeSystemMessage);
+            // Add system message for meal and sleep time constraints
+            JSONObject systemMessage1 = new JSONObject();
+            systemMessage1.put("role", "system");
+            systemMessage1.put("content", "You are a helpful assistant.");
+            messagesArray.put(systemMessage1);
 
-        // Add user message for meal time constraint
-        JSONObject mealTimeUserMessage = new JSONObject();
-        mealTimeUserMessage.put("role", "user");
-        mealTimeUserMessage.put("content", "Schedule a flexible task during meal time");
-        messagesArray.put(mealTimeUserMessage);
+            JSONObject systemMessage2 = new JSONObject();
+            systemMessage2.put("role", "system");
+            systemMessage2.put("content", "You should consider people's meal and sleep time when scheduling.");
+            messagesArray.put(systemMessage2);
 
-        // Add user messages for each flexible task
-        for (Task task : flexibleTasks) {
-            if (!task.isFixed()) {
+            // Add user message for meal times
+            JSONObject mealTimeMessage = new JSONObject();
+            mealTimeMessage.put("role", "user");
+            mealTimeMessage.put("content", "Meal times are from 12:00pm - 1:00pm and 7:00pm - 8:00pm.");
+            messagesArray.put(mealTimeMessage);
+
+            // Add user message for fixed tasks
+            for (Task task : fixedTasks) {
                 JSONObject userMessage = new JSONObject();
                 userMessage.put("role", "user");
-                userMessage.put("content", "Schedule a flexible task: " + task.getName());
+                userMessage.put("content", "Schedule a fixed task: " + task.getName() + ", Time: " + task.getTime());
                 messagesArray.put(userMessage);
             }
-        }
 
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("model", "gpt-3.5-turbo");
-        requestBody.put("messages", messagesArray);
+            // Add rest period
+            JSONObject userMessage = new JSONObject();
+            userMessage.put("role", "user");
+            userMessage.put("content", "Schedule a rest period: 30 minutes");
+            messagesArray.put(userMessage);
 
-        RequestBody body = RequestBody.create(MediaType.parse("application/json"), requestBody.toString());
+            // Add user messages for flexible tasks
+            for (Task task : flexibleTasks) {
+                JSONObject userMessageFlex = new JSONObject();
+                userMessageFlex.put("role", "user");
+                userMessageFlex.put("content", "Schedule a flexible task: " + task.getName() +
+                        ", Duration: " + task.getDuration() +
+                        ", Priority: " + task.getPriority());
+                messagesArray.put(userMessageFlex);
+            }
 
-        Request request = new Request.Builder()
-                .url(OPENAI_URL)
-                .addHeader("Authorization", "Bearer " + apiKey)
-                .post(body)
-                .build();
+            JSONObject userMessageFormat = new JSONObject();
+            userMessageFormat.put("role", "user");
+            userMessageFormat.put("content", "Provide the scheduled times for each flexible task in the format 'TaskName: StartTime - EndTime'.");
+            messagesArray.put(userMessageFormat);
 
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful()) {
-                String responseBody = response.body().string();
-                JSONObject responseJson = new JSONObject(responseBody);
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("model", "gpt-3.5-turbo");
+            requestBody.put("messages", messagesArray);
 
-                JSONArray choicesArray = responseJson.getJSONArray("choices");
+            RequestBody body = RequestBody.create(MediaType.parse("application/json"), requestBody.toString());
 
-                for (int i = 0; i < choicesArray.length(); i++) {
-                    JSONObject choiceObject = choicesArray.getJSONObject(i);
-                    String completion = choiceObject.getString("message");
-                    Long taskId = getTaskIdByCompletion(flexibleTasks, completion);
-                    String time = extractTimeFromCompletion(completion);
+            Request request = new Request.Builder()
+                    .url(OPENAI_URL)
+                    .addHeader("Authorization", "Bearer " + apiKey)
+                    .post(body)
+                    .build();
 
-                    if (taskId != null) {
-                        for (Task task : flexibleTasks) {
-                            if (task.getId().equals(taskId)) {
-                                task.setTime(time);
-                                break;
-                            }
+            try (Response response = client.newCall(request).execute()) {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    JSONObject responseJson = new JSONObject(responseBody);
+
+                    JSONArray choicesArray = responseJson.getJSONArray("choices");
+
+                    for (int i = 0; i < choicesArray.length(); i++) {
+                        JSONObject choiceObject = choicesArray.getJSONObject(i);
+                        String completion = choiceObject.getString("message");
+                        String assignedTask = getAssignedTask(completion, flexibleTasks);
+
+                        if (assignedTask != null) {
+                            System.out.println(assignedTask);
                         }
                     }
+                } else {
+                    System.out.println("Request failed. Response code: " + response.code());
                 }
-            } else {
-                System.out.println("Failed to fetch updated schedule. Error: " + response.code());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Long getTaskIdByCompletion(List<Task> flexibleTasks, String completion) {
-        // Implement logic to extract the task ID from the completion text
-        // You can use a regular expression, parsing, or any other method based on the expected format of the completion text
-        // Return the task ID if found, or null if not found
-
-        for (Task task : flexibleTasks) {
-            if (task.getName().equals(completion)) {
-                return task.getId();
-            }
-        }
-        return null;
-    }
-
-    private String extractTimeFromCompletion(String completion) {
-        // Implement logic to extract the time from the completion text
-        // Adjust the implementation based on the actual response format
-        // Assuming the completion text is in the format: "Scheduled time: 8:00am - 9:30am"
-
-        // Split the completion text by the ":" delimiter
-        String[] parts = completion.split(":");
-
-        if (parts.length > 1) {
-            // Extract the time part after the ":" delimiter
-            String timePart = parts[1].trim();
-
-            // Split the time part by the "-" delimiter
-            String[] timeRange = timePart.split("-");
-
-            if (timeRange.length > 1) {
-                // Extract the start and end time
-                String startTime = timeRange[0].trim();
-                String endTime = timeRange[1].trim();
-
-                // Return the extracted time as a formatted string
-                return startTime + " - " + endTime;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
-        // Return an empty string if extraction fails
-        return "";
+
+
+    private String getAssignedTask(String completion, List<Task> flexibleTasks) {
+        // Use a regular expression to find matches in the format "TaskName: StartTime - EndTime".
+        Pattern pattern = Pattern.compile("(.*): (.*am|pm) - (.*am|pm)");
+        Matcher matcher = pattern.matcher(completion);
+
+        // If a match is found, return the match. Otherwise, return null.
+        if (matcher.find()) {
+            return matcher.group();
+        } else {
+            return null;
+        }
     }
+
+
 }
